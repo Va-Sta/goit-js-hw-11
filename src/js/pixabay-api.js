e6aacdbb6a06bd8917fe9de598cf3ee9fa057ac4
Falka <valentyna.stashenko@gmail.com>
2025-02-16 00:12:16 +0200

    hw-11 v.1

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const API_KEY = '48865554-9c41d4f1cdb1a3126cd29aed3';
+const api_URL = 'https://pixabay.com/api/';
+
+export const searchImage = searchValue => {
+  return axios.get(
+    `${api_URL}?key=${API_KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true`
+  );
+};
---
+++ src/main.js
@@ -0,0 +1,35 @@
+import * as api from './js/pixabay-api';
+import * as render from './js/render-functions';
+
+const searchForm = document.querySelector('.search-form');
+const loader = document.querySelector('.loader-placeholder');
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const form = event.target;
+  const searchInputValue = form.elements.input.value.trim();
+  if (searchInputValue === '') {
+    render.showError('Please enter something');
+    return;
+  }
+  render.clearGallery();
+  loader.classList.add('loader');
+
+  api
+    .searchImage(searchInputValue)
+    .then(response => {
+      console.log(response);
+      if (response.data.hits.length === 0) {
+        render.showError(
+          'Sorry, there are no images matching your search query. Please, try again!'
+        );
+        return;
+      }
+      render.showGallery(response.data.hits);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(() => {
+      loader.classList.remove('loader');
+    });
+});
