80bd1824ed86fdc39735b55100cd49709e7d3c7a
Falka <valentyna.stashenko@gmail.com>
2025-02-19 15:46:51 +0200

    search via AI

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import * as api from './js/pixabay-api';
+import * as ai from './js/gemini-api';
 import * as render from './js/render-functions';
 
 const searchForm = document.querySelector('.search-form');
 const loader = document.querySelector('.loader-placeholder');
+
 searchForm.addEventListener('submit', event => {
   event.preventDefault();
   const form = event.target;
@@ -14,17 +16,27 @@ searchForm.addEventListener('submit', event => {
   render.clearGallery();
   loader.classList.add('loader');
 
-  api
-    .searchImage(searchInputValue)
+  let prompt = ai.pixabayPrompt(searchInputValue);
+
+  ai.askAI(prompt)
     .then(response => {
-      console.log(response);
-      if (response.data.hits.length === 0) {
-        render.showError(
-          'Sorry, there are no images matching your search query. Please, try again!'
-        );
-        return;
-      }
-      render.showGallery(response.data.hits);
+      let params = response.data.candidates[0].content.parts[0].text;
+      console.log(params);
+      api
+        .searchImage(params)
+        .then(response => {
+          console.log(response);
+          if (response.data.hits.length === 0) {
+            render.showError(
+              'Sorry, there are no images matching your search query. Please, try again!'
+            );
+            return;
+          }
+          render.showGallery(response.data.hits);
+        })
+        .catch(error => {
+          console.log(error);
+        });
     })
     .catch(error => {
       console.log(error);
